cc86eb8f8506f1b8c69c0595099c085d53ddb18c
husain-creat <husaindradi@gmail.com>
2023-07-26 21:03:18 +0300

    i need the models to make form done

--- wholesaleapp/urls.py
+++ wholesaleapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
+    path('', views.index),
+    path('signup', views.companyform), # this link for company form page
+    path('salesrep', views.salesrepform),# this link is for sales representative form page
+    
 
   ]
--- wholesaleapp/views.py
+++ wholesaleapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect,HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('SJHDJSDB')
+
+def companyform(request):
+    return render (request,'companyform.html')
+
+
+def salesrepform(request):
+    return render (request,'salesrep.html')
+
+
+
